4fe05fdf73eb7e6c45ab29c82af06361871db7ac
Donny Winston <dwinston@alum.mit.edu>
2013-01-29 14:27:25 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Golfballs::Application.config.secret_token = 'd8d0fe5ae68321c47c71705120def8b897f69bdf033aaf32f3412ddac2379486434b527ff1f335b016c4cf3c6062da647e710fe312108077ca54f3c27351276c'
